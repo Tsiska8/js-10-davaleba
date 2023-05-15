c9aef41206563bd395f0ea2cf00afe5aa4cbc5c5
Ketevan <qeto71@gmail.com>
2023-05-15 12:39:43 +0400

    update

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let text = "Hello world!" + "";
+function myFunction() {
+    var str = "Hello Wold!";
+    document.getElementById("demo").innerHTML = str.repeat(6);
+}
+
+
+/*let text = "Hello world!" + "";
 let result = text.repeat(4);
 
-document.getElementById("demo").innerHTML = result;
+document.getElementById("demo").innerHTML = result;*/
